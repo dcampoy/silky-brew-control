41173d55982a8bfea1730a2010471b387d244b74
Diego Campoy <diego.campoy.collado@gmail.com>
2025-01-10 11:36:11 +0000

    Trying to fix react router

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/silky-brew-control" element={<Index />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
